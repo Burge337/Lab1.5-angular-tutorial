24cce696ccfe833845243f3bd81c02d8d860d6ab
Burge337 <burge337@morris.umn.edu>
2020-02-08 21:27:06 -0600

    Added name and address fields to checkout form, added purchase button to cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,25 +11,26 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
   items;
   checkoutForm;
+
   constructor(
     private cartService: CartService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
   ) {
     this.checkoutForm = this.formBuilder.group({
       name: '',
       address: ''
-    })
+    });
   }
+
   ngOnInit() {
     this.items = this.cartService.getItems();
   }
 
-  onSubmit(customerData){
-    //Process checkout data here
+  onSubmit(customerData) {
+    // Process checkout data here
     console.warn('Your order has been submitted', customerData);
 
-  this.items = this.cartService.clearCart();
-  this.checkoutForm.resest();
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
-
 }
